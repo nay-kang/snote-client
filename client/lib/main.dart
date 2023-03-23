e1b3a32fb5fd896c381dc8ee465bcd31c63f60a6
Nay.Kang <kunvunk@gmail.com>
2023-03-23 20:12:53 +0800

    add client app to exchange with server and encrypt note content

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_quill/flutter_quill.dart' as quill;
@@ -9,6 +10,9 @@ import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:http/http.dart' as http;
+import 'package:encrypt/encrypt.dart' as encrypt;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -22,11 +26,7 @@ class SNoteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     var authProviders = [EmailAuthProvider()];
     var appState = SNoteAppState();
-    // FirebaseAuth.instance.authStateChanges().listen(
-    //   (User? user) {
-    //     user?.getIdToken().then((value) {});
-    //   },
-    // );
+
     return MaterialApp(
       initialRoute:
           FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/index',
@@ -168,12 +168,12 @@ class NoteEditor extends StatelessWidget {
     var content = note.content;
     final textController = quill.QuillController.basic();
     textController.document = quill.Document.fromJson(content);
-    textController.addListener(
-      () {
-        appState.updateContent(
-            note.id, textController.document.toDelta().toJson());
-      },
-    );
+    // textController.addListener(
+    //   () {
+    //     appState.updateContent(
+    //         note.id, textController.document.toDelta().toJson());
+    //   },
+    // );
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -227,54 +227,144 @@ class NoteEditor extends StatelessWidget {
 }
 
 class SNoteAppState extends ChangeNotifier {
-  SNoteAppState() {
-    var jsonData = loadNotes();
-    jsonData.then((value) {
-      List<String> jsonList =
-          (jsonDecode(value) as List<dynamic>).cast<String>();
-      noteList = jsonList.map((e) => NoteModel.autoId(content: e)).toList();
-      notifyListeners();
-    });
-  }
-  Future<String> loadNotes() async {
-    return await rootBundle.loadString('assets/fake_notes.json');
-  }
-
   List<NoteModel> noteList = [];
+  String? token;
+  late NoteService noteService;
+
+  SNoteAppState() {
+    FirebaseAuth.instance.authStateChanges().listen(
+      (User? user) {
+        () async {
+          token = await user?.getIdToken();
+          if (token == null) {
+            return;
+          }
+          //TODO need a solution to generate a aes key for new user
+          var seStorage = const FlutterSecureStorage();
+          var aesKeyBase64 = await seStorage.read(key: 'note_aes_key');
+          noteService = NoteService(token!, aesKeyBase64!);
+          var notes = await noteService.loadNotesHttp();
+          noteList.addAll(notes);
+          notifyListeners();
+        }();
+      },
+    );
+  }
 
   List<dynamic> getById(String id) {
     return noteList.firstWhere((element) => element.id == id).content;
   }
 
   void updateContent(String id, List<dynamic> content) {
-    noteList.firstWhere((element) => element.id == id).content = content;
+    var note = noteList.firstWhere((element) => element.id == id);
+    note.content = content;
+    noteService.updateNote(id, content).then((value) {
+      noteList.remove(note);
+      noteList.insert(0, value);
+      notifyListeners();
+    });
     notifyListeners();
   }
 
   NoteModel createNote() {
-    var note = NoteModel.autoId(content: '');
+    var note = NoteModel();
     noteList.insert(0, note);
 
     return note;
   }
 }
 
+class NoteService {
+  late encrypt.Encrypter _encrypter;
+  late String token;
+  late String aesKeyBase64;
+  static const String host = 'http://127.0.0.1:8000';
+  NoteService(this.token, this.aesKeyBase64) {
+    var aesKey = encrypt.Key.fromBase64(aesKeyBase64);
+    _encrypter = encrypt.Encrypter(encrypt.AES(aesKey));
+  }
+
+  Future<NoteModel> updateNote(String id, List<dynamic> content) async {
+    var encContentStr = _encrypt(jsonEncode(content));
+    var header = {
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+      'Content-Type': 'application/json; charset=UTF-8',
+    };
+    var response = await http.put(Uri.parse('$host/note/$id'),
+        headers: header,
+        body: jsonEncode({
+          'content': encContentStr,
+        }));
+    Map<String, dynamic> data = jsonDecode(response.body);
+    var decContent = _decrypt(data['content']);
+    var note = NoteModel(
+        id: data['id'],
+        content: jsonDecode(decContent),
+        createdAt: data['created_at'],
+        updatedAt: data['updated_at']);
+    return note;
+  }
+
+  Future<List<NoteModel>> loadNotesHttp() async {
+    var header = {
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+    };
+    var response = await http.get(Uri.parse('$host/note/'), headers: header);
+    List data = jsonDecode(response.body);
+    var notes = data.map((d) {
+      var n = (d as Map<String, dynamic>);
+      var ciphertext = n['content'];
+      var plaintext = _decrypt(ciphertext);
+      var note = NoteModel(
+          id: n['id'],
+          content: jsonDecode(plaintext),
+          createdAt: n['created_at'],
+          updatedAt: n['updated_at']);
+      return note;
+    }).toList();
+    return notes;
+  }
+
+  String _encrypt(String content) {
+    var iv = encrypt.IV.fromSecureRandom(16);
+    var encContent = _encrypter.encrypt(content, iv: iv);
+    return iv.base64 + encContent.base64;
+  }
+
+  String _decrypt(String ciphertext) {
+    var ivStr = ciphertext.substring(0, 24);
+    var iv = encrypt.IV.fromBase64(ivStr);
+    var encContentStr = ciphertext.substring(24);
+    return _encrypter.decrypt64(encContentStr, iv: iv);
+  }
+}
+
 class NoteModel {
   late String id;
   late List<dynamic> content;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+
   static const uuid = Uuid();
-  NoteModel.autoId({required String content}) {
-    id = uuid.v1();
-    this.content = convertContent(content);
-  }
 
-  NoteModel({required this.id, required String content}) {
-    this.content = convertContent(content);
-  }
-
-  List<dynamic> convertContent(String content) {
-    var deltaNote = quill.Delta();
-    deltaNote.insert('$content\n');
-    return deltaNote.toJson();
+  NoteModel(
+      {String? id,
+      List<dynamic>? content,
+      String? createdAt,
+      String? updatedAt}) {
+    id ??= uuid.v4();
+    this.id = id;
+    if (content == null) {
+      var delta = quill.Delta();
+      delta.insert('\n');
+      content = delta.toJson();
+    }
+    this.content = content;
+    if (createdAt != null) {
+      this.createdAt = DateTime.parse(createdAt);
+    }
+    if (updatedAt != null) {
+      this.updatedAt = DateTime.parse(updatedAt);
+    }
   }
 }
--- client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import desktop_webview_auth
 import device_info_plus
 import firebase_auth
 import firebase_core
+import flutter_secure_storage_macos
 import pasteboard
 import path_provider_foundation
 import url_launcher_macos
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
