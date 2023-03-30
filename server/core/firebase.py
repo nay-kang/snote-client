615d1936bfc1dcc9920ec8412ece13fef3e34795
nay <kunvunk@gmail.com>
2023-03-30 17:45:00 +0800

    seperate client and server

--- server/core/firebase.py
+++
@@ -1,5 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials,auth
-
-cred = credentials.Certificate('firebase_account.json')
-firebase_admin.initialize_app(cred)
--- server/core/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from core import models
-
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Note
-        fields = ['id','uid','content','created_at','updated_at']
